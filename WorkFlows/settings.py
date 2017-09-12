025591e5431a8b16f1969ed0b780b17fa14a10d7
Rahul Kumar <rkrjob55@gmail.com>
2017-09-12 08:39:35 +0530

    update

--- WorkFlows/settings.py
+++ WorkFlows/settings.py
@@ -131,12 +131,12 @@ REST_FRAMEWORK = {
 REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        #'rest_framework.authentication.TokenAuthentication',
-        #'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
 
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        #'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
